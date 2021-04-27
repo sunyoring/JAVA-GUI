a77632418b1dbdad04ced26017625bebf6ec9757
Sunyoring <sunhyo67@gmail.com>
2021-04-27 21:37:20 +0900

    2021-04-27

--- src/com/sunyo/rockpaperscissors/가위바위보.java
+++ src/com/sunyo/rockpaperscissors/가위바위보.java
@@ -29,7 +29,7 @@ public class 가위바위보 extends JFrame {
 	private ImageIcon rock = new ImageIcon("src/images/rock.png");
 	private ImageIcon paper = new ImageIcon("src/images/paper.png");
 	private ImageIcon scissor = new ImageIcon("src/images/scissors.png");
-	private Image player2 = new ImageIcon("src/images/scissors.png").getImage();
+	private static Image player2 = new ImageIcon("src/images/paper.png").getImage();
 	private JScrollPane scrollPane;
 	private ImageIcon icon;
 
@@ -40,37 +40,35 @@ public class 가위바위보 extends JFrame {
 	BufferedImage img = null;
 	private String result;
 
-	private int player2X = 350;
-	private int player2Y = 100;
+	private static int player2X = 350;
+	private static int player2Y = 100;
 	private int buttonY = 680;
 	private int scissorX = 600;
 	private int paperX = 350;
 	private int rockX = 105;
-	
+
+	private boolean attack = false;
 
 	public 가위바위보() {
-		
-	
+
 		setTitle("가위바위보 게임");
-	
+
 		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setSize(1000,1000);
+		layeredPane.setSize(1000, 1000);
 		layeredPane.setLayout(null);
 
-
-		
 		try {
 			img = ImageIO.read(new File("src/images/RPSMain.png"));
-		}catch(IOException e) {
+		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, "이미지 불러오기 실패");
 			System.exit(0);
 		}
 		MyPanel panel = new MyPanel();
-		panel.setSize(1000,1000);
-		
+		panel.setSize(1000, 1000);
+
 		setLayout(null);
-		
-		setBounds(0,0,1000,1000);
+
+		setBounds(0, 0, 1000, 1000);
 		setVisible(true);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -98,13 +96,11 @@ public class 가위바위보 extends JFrame {
 			public void mousePressed(MouseEvent e) {
 				paperButton.setVisible(false);
 				scissorButton.setVisible(false);
-				while(rockX < 350) {
-					rockX =+10;
-				}
+				attack = true;
 			}
 		});
 		layeredPane.add(rockButton);
-		
+
 		paperButton.setBounds(paperX, buttonY, 250, 250);
 		paperButton.setBorderPainted(false);
 		paperButton.setContentAreaFilled(false);
@@ -115,22 +111,24 @@ public class 가위바위보 extends JFrame {
 //				paperButton.setIcon(); // 마우스가 내려왔을 때 다시 이미지를 바꿔준다.
 				paperButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); // 마우스 커서모양 변경
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 //				paperButton.setIcon(); // 마우스가 내려왔을 때 다시 이미지를 바꿔준다.
 				paperButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				rockButton.setVisible(false);
 				scissorButton.setVisible(false);
+				attack = true;
+
 			}
 		});
 		layeredPane.add(paperButton);
-		
+
 		scissorButton.setBounds(scissorX, buttonY, 250, 250);
 		scissorButton.setBorderPainted(false);
 		scissorButton.setContentAreaFilled(false);
@@ -141,29 +139,28 @@ public class 가위바위보 extends JFrame {
 //				scissorButton.setIcon(); // 마우스가 내려왔을 때 다시 이미지를 바꿔준다.
 				scissorButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); // 마우스 커서모양 변경
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 //				scissorButton.setIcon(); // 마우스가 내려왔을 때 다시 이미지를 바꿔준다.
 				scissorButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				paperButton.setVisible(false);
 				rockButton.setVisible(false);
-
+				attack = true;
 			}
 		});
-		layeredPane.add(scissorButton);
-
-			
+		layeredPane.add(scissorButton);		
 		add(layeredPane);
 		layeredPane.add(panel);
 
-		
 	}
+
+
 	
 	class MyPanel extends JPanel {
 
@@ -172,21 +169,29 @@ public class 가위바위보 extends JFrame {
 			g.drawImage(player2, player2X, player2Y, null);
 
 		}
+	
 	}
+
+
 
 	public void player2Attack() {
-		int Atk = (int) (Math.random() * 3) + 1;
-		if (Atk == 1) {
-			Image player2 = new ImageIcon("src/images/rock.png").getImage();
-		}
-		if (Atk == 2) {
-			Image player2 = new ImageIcon("src/images/paper.png").getImage();
-		}
-		if (Atk == 3) {
-			Image player2 = new ImageIcon("src/images/scissors.png").getImage();
-		}
-	}
 
+		int Atk = (int) (Math.random() * 3) + 1;
+		switch (Atk) {
+		case 1:
+			player2 = new ImageIcon("src/images/rock.png").getImage();
+			break;
+		case 2:
+			player2 = new ImageIcon("src/images/paper.png").getImage();
+			break;
+		case 3:
+			player2 = new ImageIcon("src/images/scissors.png").getImage();
+			break;
+		default:
+
+		}
+
+	}
 
 	public static void main(String[] args) {
 		new 가위바위보();
