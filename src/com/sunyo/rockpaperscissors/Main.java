a114e0afe8aae7d4a0c677b18ed25874030eaf1e
Sunyoring <sunhyo67@gmail.com>
2021-04-28 19:31:00 +0900

    2021-04-28

--- src/com/sunyo/rockpaperscissors/Main.java
+++ src/com/sunyo/rockpaperscissors/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static final int SCREEN_HEIGHT = 1000;
 	
 	public static void main(String[] args) {
-		new 가위바위보3();
+		new 가위바위보();
 	}
 
 }
--- src/com/sunyo/rockpaperscissors/MyPanel.java
+++
@@ -1,7 +0,0 @@
-package com.sunyo.rockpaperscissors;
-
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-
-import javax.swing.JPanel;
-
