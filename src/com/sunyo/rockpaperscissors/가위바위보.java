1fec595f3c2cc11d166839a48817234435422153
Sunyoring <sunhyo67@gmail.com>
2021-04-28 22:12:11 +0900

    2021-04-28

--- src/com/sunyo/rockpaperscissors/가위바위보.java
+++ src/com/sunyo/rockpaperscissors/가위바위보.java
@@ -2,7 +2,9 @@ package com.sunyo.rockpaperscissors;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -17,8 +19,10 @@ public class 가위바위보 extends JFrame {
 	private Graphics screenGraphic;
 
 	private Image background = new ImageIcon("src/images/RPSMain.png").getImage();
-	private Image player2 = new ImageIcon("src/images/rock.png").getImage();
-	
+	private Image player2rockImage = new ImageIcon("src/images/rock.png").getImage();
+	private Image player2scissorImage = new ImageIcon("src/images/scissors.png").getImage();
+	private Image player2paperImage = new ImageIcon("src/images/paper.png").getImage();
+
 	private ImageIcon back = new ImageIcon("src/images/back.png");
 	private ImageIcon rock = new ImageIcon("src/images/rock.png");
 	private ImageIcon paper = new ImageIcon("src/images/paper.png");
@@ -27,10 +31,12 @@ public class 가위바위보 extends JFrame {
 	private ImageIcon player2paper = new ImageIcon("src/images/paper.png");
 	private ImageIcon player2scissor = new ImageIcon("src/images/scissors.png");
 	private ImageIcon imageException = new ImageIcon("src/images/imageException.png");
+	private ImageIcon exitButtonImage = new ImageIcon("src/images/exitButton.png");
 
 	private JButton rockButton = new JButton(rock);
 	private JButton paperButton = new JButton(paper);
 	private JButton scissorButton = new JButton(scissor);
+	private JButton exitButton = new JButton(exitButtonImage);
 
 	private JButton reset = new JButton(back);
 
@@ -45,7 +51,7 @@ public class 가위바위보 extends JFrame {
 	private int paperX = 350;
 	private int rockX = 105;
 	private boolean choice = false;
-	private int[] randomNum = new int[20];
+	private int[] randomNum = new int[30];
 	private JButton[] randomBut = new JButton[20];
 	private JButton player2Button;
 
@@ -53,6 +59,8 @@ public class 가위바위보 extends JFrame {
 	private boolean rockB = false;
 	private boolean paperB = false;
 	private boolean scissorB = false;
+	private String result = "WIN !!";
+	private Image player2;
 
 	public 가위바위보() {
 		setUndecorated(true);
@@ -65,6 +73,7 @@ public class 가위바위보 extends JFrame {
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null); // 버튼이나 JLabel같은 것을 넣을 때 제 위치에 들어가게 해줌.
 
+		player2 = ranImage();
 
 		rockButton.setBounds(rockX, buttonY, 250, 250);
 		rockButton.setBorderPainted(false);
@@ -88,7 +97,13 @@ public class 가위바위보 extends JFrame {
 				scissorButton.setVisible(false);
 				paperButton.setVisible(false);
 				choice = true;
-				
+				if (player2 == player2scissorImage) {
+					result = "WIN !!";
+				} else if (player2 == player2paperImage) {
+					result = "LOSE";
+				} else if (player2 == player2rockImage) {
+					result = "DRAW";
+				}
 
 			}
 		});
@@ -116,6 +131,13 @@ public class 가위바위보 extends JFrame {
 				scissorButton.setVisible(false);
 				rockButton.setVisible(false);
 				choice = true;
+				if (player2 == player2scissorImage) {
+					result = "LOSE";
+				} else if (player2 == player2paperImage) {
+					result = "DRAW";
+				} else if (player2 == player2rockImage) {
+					result = "WIN !!";
+				}
 
 			}
 		});
@@ -143,9 +165,17 @@ public class 가위바위보 extends JFrame {
 				paperButton.setVisible(false);
 				rockButton.setVisible(false);
 				choice = true;
+				if (player2 == player2scissorImage) {
+					result = "DRAW";
+				} else if (player2 == player2paperImage) {
+					result = "WIN !!";
+				} else if (player2 == player2rockImage) {
+					result = "LOSE";
+				}
 
 			}
 		});
+
 		add(scissorButton);
 
 		reset.setBounds(50, 450, 70, 60);
@@ -174,11 +204,35 @@ public class 가위바위보 extends JFrame {
 		});
 		add(reset);
 
+		exitButton.setBounds(850, 10, 150, 40);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); // 마우스 커서모양 변경
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.exit(0);
+
+			}
+		});
+		add(exitButton);
+
 		for (int i = 0; i < randomNum.length; i++) {
-			randomNum[i] = (int) (Math.random() * 3) + 1;
+			randomNum[i] = (int) (Math.random() * 6) + 1;
 		}
 	}
-
 
 	public void resetGame() {
 		paperButton.setVisible(true);
@@ -192,16 +246,19 @@ public class 가위바위보 extends JFrame {
 	public void paint(Graphics g) { // paint()는 JFrame을 상속받은 GUI게임에서 가장 앞 부분에 화면을 그려주는 메소드로써 약속된 부분이다.
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT); //
 		screenGraphic = screenImage.getGraphics(); // 스크린 이미지를 통해 그래픽 객체를 얻어옴.
-		screenDraw((Graphics) screenGraphic); // 스크린 그래픽에 어떠한 그림을 그려준다.
+		screenDraw((Graphics2D) screenGraphic); // 스크린 그래픽에 어떠한 그림을 그려준다.
 		g.drawImage(screenImage, 0, 0, null); // 스크린 이미지를 (0,0)위치에 그려준다.
 	}
 
-	public void screenDraw(Graphics g) {
+	public void screenDraw(Graphics2D g) {
 		g.drawImage(background, 0, 0, null); // background를 스크린 이미지에 그려준다.
 		// 단순 이미지 출력.
 
-		if (choice) {
+		if (choice == true) {
 			g.drawImage(player2, player2X, player2Y, null);
+			g.setFont(new Font("Arial", Font.BOLD, 100));
+			g.setColor(Color.DARK_GRAY);
+			g.drawString(result, paperX, 500);
 
 		}
 
@@ -210,17 +267,29 @@ public class 가위바위보 extends JFrame {
 		this.repaint();
 	} // 더블 버퍼링기법.
 
+	public Image ranImage() {
 
-	public void ranImage() {
+		if (randomNum[(int) ((Math.random() * 29) + 1)] == 1) {
 
-		if (randomNum[(int) ((Math.random() * 19) + 1)] == 1) {
-			player2 = new ImageIcon("src/images/rock.png").getImage();
-		} else if (randomNum[(int) (Math.random() * 19) + 1] == 2) {
-			player2 = new ImageIcon("src/images/paper.png").getImage();
-		} else if (randomNum[(int) (Math.random() * 19) + 1] == 3) {
-			player2 = new ImageIcon("src/images/scissors.png").getImage();
-		} else
-			player2 = new ImageIcon("src/images/scissors.png").getImage();
+			return player2 = player2rockImage;
+		} else if (randomNum[(int) (Math.random() * 29) + 1] == 2) {
 
+			return player2 = player2paperImage;
+		} else if (randomNum[(int) (Math.random() * 29) + 1] == 3) {
+
+			return player2 = player2scissorImage;
+
+		} else if (randomNum[(int) ((Math.random() * 29) + 1)] == 4) {
+
+			return player2 = player2rockImage;
+		} else if (randomNum[(int) (Math.random() * 29) + 1] == 5) {
+
+			return player2 = player2paperImage;
+		} else if (randomNum[(int) (Math.random() * 29) + 1] == 6) {
+
+			return player2 = player2scissorImage;
+
+		}
+		return player2paperImage;
 	}
 }
