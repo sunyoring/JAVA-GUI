f489ccbb0d8c499e0f90ecdb3f6e7b7f8a8078cd
Sunyoring <sunhyo67@gmail.com>
2021-04-29 22:25:44 +0900

    2021-04-29

--- src/com/sunyo/calculator/Calculator.java
+++ src/com/sunyo/calculator/Calculator.java
@@ -4,5 +4,13 @@ import javax.swing.JFrame;
 
 public class Calculator extends JFrame {
 	
-	
+	public Calculator() {
+		setTitle("계산기");
+		setSize(400,600);
+		setVisible(true);
+		setLocationRelativeTo(null);// null : 실행 시 가운데에 창이 뜸
+		setResizable(false); // 창의 크기를 조절할 수 없게 함.
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+		
 }
--- src/com/sunyo/calculator/Main.java
+++ src/com/sunyo/calculator/Main.java
@@ -3,8 +3,7 @@ package com.sunyo.calculator;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Calculator();
 	}
 
 }
